06d7bc917ca03946d72c1be3f5490616f23858a7
Ferrari.20190 <ferrari.michele.studente.181@itispaleocapa.it>
2023-03-01 13:52:41 +0100

    <funzioni>

--- ferrari_win_form2/Form1.Designer.cs
+++ ferrari_win_form2/Form1.Designer.cs
@@ -29,33 +29,184 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.titolo1 = new System.Windows.Forms.Label();
+            this.labelnome = new System.Windows.Forms.Label();
+            this.labelprezzo = new System.Windows.Forms.Label();
+            this.textnome = new System.Windows.Forms.TextBox();
+            this.textprezzo = new System.Windows.Forms.TextBox();
+            this.buttonaggiunta = new System.Windows.Forms.Button();
+            this.buttoncancella = new System.Windows.Forms.Button();
+            this.buttonser = new System.Windows.Forms.Button();
+            this.buttonmod = new System.Windows.Forms.Button();
+            this.textmodprezzo = new System.Windows.Forms.TextBox();
+            this.textmodnome = new System.Windows.Forms.TextBox();
+            this.labelmodprezzo = new System.Windows.Forms.Label();
+            this.labelmodnome = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(468, 12);
+            this.listView1.Location = new System.Drawing.Point(442, 12);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(320, 426);
+            this.listView1.Size = new System.Drawing.Size(346, 426);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // titolo1
+            // 
+            this.titolo1.AutoSize = true;
+            this.titolo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.titolo1.Location = new System.Drawing.Point(12, 12);
+            this.titolo1.Name = "titolo1";
+            this.titolo1.Size = new System.Drawing.Size(224, 31);
+            this.titolo1.TabIndex = 1;
+            this.titolo1.Text = "Gestione Prodotti";
+            // 
+            // labelnome
+            // 
+            this.labelnome.AutoSize = true;
+            this.labelnome.Location = new System.Drawing.Point(15, 54);
+            this.labelnome.Name = "labelnome";
+            this.labelnome.Size = new System.Drawing.Size(38, 13);
+            this.labelnome.TabIndex = 2;
+            this.labelnome.Text = "Nome:";
+            // 
+            // labelprezzo
+            // 
+            this.labelprezzo.AutoSize = true;
+            this.labelprezzo.Location = new System.Drawing.Point(136, 54);
+            this.labelprezzo.Name = "labelprezzo";
+            this.labelprezzo.Size = new System.Drawing.Size(42, 13);
+            this.labelprezzo.TabIndex = 3;
+            this.labelprezzo.Text = "Prezzo:";
+            // 
+            // textnome
+            // 
+            this.textnome.Location = new System.Drawing.Point(18, 70);
+            this.textnome.Name = "textnome";
+            this.textnome.Size = new System.Drawing.Size(100, 20);
+            this.textnome.TabIndex = 4;
+            // 
+            // textprezzo
+            // 
+            this.textprezzo.Location = new System.Drawing.Point(139, 70);
+            this.textprezzo.Name = "textprezzo";
+            this.textprezzo.Size = new System.Drawing.Size(100, 20);
+            this.textprezzo.TabIndex = 5;
+            // 
+            // buttonaggiunta
+            // 
+            this.buttonaggiunta.Location = new System.Drawing.Point(18, 108);
+            this.buttonaggiunta.Name = "buttonaggiunta";
+            this.buttonaggiunta.Size = new System.Drawing.Size(100, 23);
+            this.buttonaggiunta.TabIndex = 6;
+            this.buttonaggiunta.Text = "Aggiunta";
+            this.buttonaggiunta.UseVisualStyleBackColor = true;
+            this.buttonaggiunta.Click += new System.EventHandler(this.buttonaggiunta_Click_1);
+            // 
+            // buttoncancella
+            // 
+            this.buttoncancella.Location = new System.Drawing.Point(139, 108);
+            this.buttoncancella.Name = "buttoncancella";
+            this.buttoncancella.Size = new System.Drawing.Size(100, 23);
+            this.buttoncancella.TabIndex = 7;
+            this.buttoncancella.Text = "Cancella";
+            this.buttoncancella.UseVisualStyleBackColor = true;
+            // 
+            // buttonser
+            // 
+            this.buttonser.Location = new System.Drawing.Point(139, 295);
+            this.buttonser.Name = "buttonser";
+            this.buttonser.Size = new System.Drawing.Size(100, 23);
+            this.buttonser.TabIndex = 13;
+            this.buttonser.Text = "Ricerca";
+            this.buttonser.UseVisualStyleBackColor = true;
+            // 
+            // buttonmod
+            // 
+            this.buttonmod.Location = new System.Drawing.Point(18, 295);
+            this.buttonmod.Name = "buttonmod";
+            this.buttonmod.Size = new System.Drawing.Size(100, 23);
+            this.buttonmod.TabIndex = 12;
+            this.buttonmod.Text = "Modifica";
+            this.buttonmod.UseVisualStyleBackColor = true;
+            // 
+            // textmodprezzo
+            // 
+            this.textmodprezzo.Location = new System.Drawing.Point(139, 257);
+            this.textmodprezzo.Name = "textmodprezzo";
+            this.textmodprezzo.Size = new System.Drawing.Size(100, 20);
+            this.textmodprezzo.TabIndex = 11;
+            // 
+            // textmodnome
+            // 
+            this.textmodnome.Location = new System.Drawing.Point(18, 257);
+            this.textmodnome.Name = "textmodnome";
+            this.textmodnome.Size = new System.Drawing.Size(100, 20);
+            this.textmodnome.TabIndex = 10;
+            // 
+            // labelmodprezzo
+            // 
+            this.labelmodprezzo.AutoSize = true;
+            this.labelmodprezzo.Location = new System.Drawing.Point(136, 241);
+            this.labelmodprezzo.Name = "labelmodprezzo";
+            this.labelmodprezzo.Size = new System.Drawing.Size(42, 13);
+            this.labelmodprezzo.TabIndex = 9;
+            this.labelmodprezzo.Text = "Prezzo:";
+            // 
+            // labelmodnome
+            // 
+            this.labelmodnome.AutoSize = true;
+            this.labelmodnome.Location = new System.Drawing.Point(15, 241);
+            this.labelmodnome.Name = "labelmodnome";
+            this.labelmodnome.Size = new System.Drawing.Size(38, 13);
+            this.labelmodnome.TabIndex = 8;
+            this.labelmodnome.Text = "Nome:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonser);
+            this.Controls.Add(this.buttonmod);
+            this.Controls.Add(this.textmodprezzo);
+            this.Controls.Add(this.textmodnome);
+            this.Controls.Add(this.labelmodprezzo);
+            this.Controls.Add(this.labelmodnome);
+            this.Controls.Add(this.buttoncancella);
+            this.Controls.Add(this.buttonaggiunta);
+            this.Controls.Add(this.textprezzo);
+            this.Controls.Add(this.textnome);
+            this.Controls.Add(this.labelprezzo);
+            this.Controls.Add(this.labelnome);
+            this.Controls.Add(this.titolo1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label titolo1;
+        private System.Windows.Forms.Label labelnome;
+        private System.Windows.Forms.Label labelprezzo;
+        private System.Windows.Forms.TextBox textnome;
+        private System.Windows.Forms.TextBox textprezzo;
+        private System.Windows.Forms.Button buttonaggiunta;
+        private System.Windows.Forms.Button buttoncancella;
+        private System.Windows.Forms.Button buttonser;
+        private System.Windows.Forms.Button buttonmod;
+        private System.Windows.Forms.TextBox textmodprezzo;
+        private System.Windows.Forms.TextBox textmodnome;
+        private System.Windows.Forms.Label labelmodprezzo;
+        private System.Windows.Forms.Label labelmodnome;
     }
 }
 
--- ferrari_win_form2/Form1.cs
+++ ferrari_win_form2/Form1.cs
@@ -12,9 +12,131 @@ namespace ferrari_win_form2
 {
     public partial class Form1 : Form
     {
+        #region Dichiarazioni
+        public struct prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+        public prodotto[] p;
+        public int dim;
+        public float somma;
+        #endregion
+        #region Funzioni evento
         public Form1()
         {
             InitializeComponent();
+            p = new prodotto[100];
+            dim = 0;
         }
+        private void buttonaggiunta_Click_1(object sender, EventArgs e)
+        {
+            if (dim < p.Length)
+            {
+                p[dim].nome = textnome.Text;
+                p[dim].prezzo = float.Parse(textprezzo.Text);
+                dim++;
+                Visualizza(p);
+            }
+            else
+            {
+                MessageBox.Show("Array pieno!", "Errore!");
+            }
+        }
+        private void buttoncancella_Click(object sender, EventArgs e)
+        {
+            CancellaS(textnome.Text, p, ref dim);
+        }
+        private void buttonmod_Click(object sender, EventArgs e)
+        {
+            Modifica(textmodnome.Text, float.Parse(textmodprezzo.Text), p);
+        }
+        private void buttonsomma_Click(object sender, EventArgs e)
+        {
+            SommaProdotti(ref dim);
+        }
+        #endregion
+
+        #region Funzioni servizio
+        public string prodString(prodotto p)
+        {
+            return "Nome: " + p.nome + " prezzo: " + p.prezzo.ToString("0.00");
+        }
+        public void Visualizza(prodotto[] p)
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                listView1.Items.Add(prodString(p[i]));
+            }
+        }
+        public int RicercaS(string e, prodotto[] p)
+        {
+            int risultatoricerca = 0;
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i].nome == e)
+                {
+                    risultatoricerca = i;
+                    break;
+                }
+                else
+                {
+                    risultatoricerca = -1;
+                }
+            }
+            return risultatoricerca;
+        }
+        public void CancellaS(string e, prodotto[] p, ref int dim)
+        {
+            var rispCanc = MessageBox.Show("È sicuro di voler eliminare l'elemento?", "Conferma rimozione elemento", MessageBoxButtons.YesNo);
+            if (rispCanc == DialogResult.Yes)
+            {
+                if (RicercaS(textnome.Text, p) == -1)
+                {
+                    MessageBox.Show("Elemento non trovato!", "Errore!");
+                }
+                else
+                {
+                    for (int j = RicercaS(textnome.Text, p); j < dim - 1; j++)
+                    {
+                        p[j] = p[j + 1];
+                    }
+                    dim--;
+                    listView1.Clear();
+                    Visualizza(p);
+                    MessageBox.Show("Elemento eliminato correttamente!");
+                }
+            }
+        }
+        public void Modifica(string e, float pos, prodotto[] p)
+        {
+            var rispMod = MessageBox.Show("È sicuro di voler modificare l'elemento?", "Conferma modifica elemento", MessageBoxButtons.YesNo);
+            if (rispMod == DialogResult.Yes)
+            {
+                if (RicercaS(textnome.Text, p) == -1)
+                {
+                    MessageBox.Show("Elemento non trovato!", "Errore!");
+                }
+                else
+                {
+                    p[RicercaS(textnome.Text, p)].nome = e;
+                    p[RicercaS(textprezzo.Text, p)].prezzo = pos;
+                    listView1.Clear();
+                    Visualizza(p);
+                    MessageBox.Show("Elemento modificato correttamente!");
+                }
+            }
+        }
+        public void SommaProdotti(ref int dim)
+        {
+            somma = 0;
+            for (int i = 0; i < dim; i++)
+            {
+                somma += p[i].prezzo;
+            }
+            MessageBox.Show($"La somma dei prezzi dei prodotti è di {somma.ToString("0.00")}€", "Somma prodotti");
+        }
+        #endregion
     }
 }
